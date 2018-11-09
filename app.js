5f33d9640a3626b241d29a26fc920914af8af912
Peter Hsu <findlimit@gmail.com>
2018-11-09 22:31:09 +0800

    Fix not use .dotnev in heroku

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config({ silent: true });
 const Slackbot = require('slackbots');
 const express = require('express');
 const bodyParser = require('body-parser');
